4493f8e6ae38cb9791a36fe0a23d15e9d09d1d98
crabbygrindall90@gmail.com <crabbygrindall90@gmail.com>
2016-06-27 09:18:41 -0400

    Started joints, and marching cubes

---
+++ VelocityRaptor/include/vrJoint.h
@@ -0,0 +1,71 @@
+/*
+* Copyright (c) 2016 Cormac Grindall (Mithreindeir)
+*
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 1. The origin of this software must not be misrepresented; you must not
+* claim that you wrote the original software. If you use this software
+* in a product, an acknowledgment in the product documentation would be
+* appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+* misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+#ifndef HEADER_VRJOINT
+#define HEADER_VRJOINT
+
+#include "vrMath.h"
+#include "vrRigidBody.h"
+typedef void(*vrJointPreSolveFunc)(vrJoint* joint);
+typedef void(*vrJointSolveVelocityFunc)(vrJoint* joint);
+typedef void(*vrJointPostSolveFunc)(vrJoint* joint);
+typedef void(*vrJointSolvePositionFunc)(vrJoint* joint);
+
+///Point relative to body's center, changes as body rotates
+typedef struct vrLocalPoint
+{
+	///Initial body orientation
+	vrFloat initialOrientation;
+	///Point relative to body initially
+	vrVec2 initialPoint;
+	///Body which the point is on
+	vrRigidBody* body;
+} vrLocalPoint;
+
+///Empty Joint constraint
+typedef struct vrJoint
+{
+	///One body in the joint
+	vrRigidBody* A;
+	///One body in the joint
+	vrRigidBody* B;
+
+	///Local point on body A
+	vrLocalPoint anchorA;
+	///Local point on body B
+	vrLocalPoint anchorB;
+
+	///Function to pre solve constraint
+	vrJointPreSolveFunc preSolve;
+	///Function to solve velocity constraint
+	vrJointSolveVelocityFunc solveVelocity;
+	///Function to pre solve position constraint, after velocity 
+	vrJointPostSolveFunc postSolve;
+	///Function to solve position constraint
+	vrJointSolvePositionFunc solvePosition;
+} vrJoint;
+
+///Allocates memory for a joint
+vrJoint* vrJointAlloc();
+///Initializes joint
+vrJoint* vrJointInit(vrJoint* joint);
+
+///Returns a local point
+vrLocalPoint vrLocalPointInit(vrRigidBody* body, vrVec2 point);
+///Updates the local point and returns it
+vrVec2 vrGetLocalPoint(vrLocalPoint* localPoint);
+#endif
---
+++ VelocityRaptor/src/vrJoint.c
@@ -0,0 +1,58 @@
+/*
+* Copyright (c) 2016 Cormac Grindall (Mithreindeir)
+*
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 1. The origin of this software must not be misrepresented; you must not
+* claim that you wrote the original software. If you use this software
+* in a product, an acknowledgment in the product documentation would be
+* appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+* misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+#include "../include/vrJoint.h"
+
+vrJoint * vrJointAlloc()
+{
+	return vrAlloc(sizeof(vrJoint));
+}
+
+vrJoint * vrJointInit(vrJoint * joint)
+{
+	joint->A = NULL;
+	joint->B = NULL;
+	joint->preSolve = NULL;
+	joint->postSolve = NULL;
+	joint->solveVelocity = NULL;
+	joint->solvePosition = NULL;
+	return joint;
+}
+
+vrLocalPoint vrLocalPointInit(vrRigidBody * body, vrVec2 point)
+{
+	vrLocalPoint localPoint;
+	localPoint.body = body;
+	localPoint.initialOrientation = body->orientation;
+	localPoint.initialPoint = vrSub(point, body->shape->getCenter(body->shape->shape);
+	return localPoint;
+}
+
+vrVec2 vrGetLocalPoint(vrLocalPoint * localPoint)
+{
+	vrFloat newO = localPoint->body->orientation;
+	vrFloat rotation = newO - localPoint->initialOrientation;
+	if (rotation == 0) return localPoint->initialPoint;
+	vrFloat ca = VR_COS(rotation);
+	vrFloat sa = VR_SIN(rotation);
+	vrVec2 p = localPoint->initialPoint;
+	p = vrVect(p.x * ca - p.y * sa, p.x * sa + p.y * ca);
+	localPoint->initialPoint = p;
+	localPoint->initialOrientation = newO;
+
+	return p;
+}
